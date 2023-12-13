dd4bdcb1eab5c99ca56ee680ab893e5a4a52c58c
牛肉拌面 <hardews@qq.com>
2023-12-13 10:50:08 +0800

    fix: Cors middleware

--- src/main/java/com/hardews/jizhang/config/InterceptorConfig.java
+++ src/main/java/com/hardews/jizhang/config/InterceptorConfig.java
@@ -1,8 +1,8 @@
 package com.hardews.jizhang.config;
 
-import com.hardews.jizhang.component.Cors;
 import com.hardews.jizhang.component.Verify;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,8 +14,18 @@ public class InterceptorConfig implements WebMvcConfigurer {
         registry.addInterceptor(new Verify())
                 .addPathPatterns("/**")
                 .excludePathPatterns("/jizhang/user/login","/jizhang/user/login/email","/jizhang/user/register", "/jizhang/user/code");
+    }
 
-        registry.addInterceptor(new Cors())
-                .addPathPatterns("/**");
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        //允许跨域访问资源定义
+        registry.addMapping("/**")
+                // 允许发送凭证: 前端如果配置改属性为true之后，则必须同步配置
+                .allowCredentials(true)
+                // 允许所有方法
+                .allowedMethods("*")
+
+                .allowedHeaders("*");
     }
 }
