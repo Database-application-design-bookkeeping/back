c9bdc956ce893cd95c25da64b0d79af70a3a9861
牛肉拌面 <hardews@qq.com>
2023-12-13 10:12:28 +0800

    feat: add cors config class

--- src/main/java/com/hardews/jizhang/config/GlobalCorsConfig.java
+++ src/main/java/com/hardews/jizhang/config/GlobalCorsConfig.java
@@ -13,15 +13,15 @@ public class GlobalCorsConfig {
         //1. 添加 CORS配置信息
         CorsConfiguration config = new CorsConfiguration();
         //放行哪些原始域
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("");
         //是否发送 Cookie
         config.setAllowCredentials(true);
         //放行哪些请求方式
-        config.addAllowedMethod("*");
+        config.addAllowedMethod("");
         //放行哪些原始请求头部信息
-        config.addAllowedHeader("*");
+        config.addAllowedHeader("");
         //暴露哪些头部信息
-        config.addExposedHeader("*");
+        config.addExposedHeader("");
         //2. 添加映射路径
         UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
         corsConfigurationSource.registerCorsConfiguration("/**",config);
